8109dda18778ab7236514bbf03208bb83e5f75a8
ezeee <ezzequielaalejandro@gmail.com>
2026-06-06 21:15:46 -0300

    punto 1 terminado

--- src/punto1.cpp
+++ src/punto1.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
 int sensores [4] = {1,2,3,4};
-int seg_init7 [7];
+int seg_init7 [7] = {5,6,7,8,9,10,11};
 int seg7 [6] [7] = {
     {0,1,1,0,0,0,0}, //1
     {1,1,0,1,1,0,1}, //2
@@ -12,8 +12,17 @@ int seg7 [6] [7] = {
 };
 
 int boton = 10;
+int t_previo3 = 0, t_delay3 = 1000;
 int t_previo2 = 0, t_delay2 = 500;
 int t_previo = 0, t_delay = 10000;
+
+volatile int estado_de_produccion = 0;   // estado_de_produccion = 0 "quiere decir que la linea esta trabajando"
+                                         // estado_de_produccion = 1 "quiere decir que la linea ya termino"
+
+
+void IRAM_ATTR toggle (){   //QUE SUCEDE SI SE ACTIVA LA INTERRUPCION
+    estado_de_produccion = 0;
+} 
 
 
 void setup() {
@@ -26,53 +35,66 @@ void setup() {
         pinMode (seg_init7 [i], OUTPUT);
     }
 
-    pinMode (boton, INPUT);
+    pinMode (boton, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt (boton), toggle, FALLING);
 }
 
 void loop(){
 
-    
+    int t_actual = millis();
+    int t_actual2 = millis();
+
+if(estado_de_produccion = 0){
+
     for(int i = 0; i < 4; i++){
         
         if(digitalRead (sensores [i])){
-            for(int j = 0; j < 7; j++){
+            for(int j = 0; j <= 7; j++){
 
                 if(seg7 [i] [j]){                   //que sensor esta activado
-                    digitalWrite (seg7 [i] [j], HIGH);
+                    digitalWrite (seg_init7 [j], HIGH);
                 }else{
-                    digitalWrite (seg7 [i] [j], LOW);
+                    digitalWrite (seg_init7 [j], LOW);
                 }
-
-                if(sensores [3]){       //si el sensor 4 esta encendido
-
-                
-                    int t_actual = millis ();   //pasan 10 segundos
-                    t_delay=t_actual+10000;
-                    t_previo=t_actual;
-                    while(t_actual - t_previo <= t_delay){
-                  
-                  }
-
-                  while (digitalRead (boton) == 0){
-
-                      for(int j = 0; j < 7; j++){  //muestra la C
-                   if(seg7 [4] [j]){
-                       digitalWrite (seg7 [4] [j], HIGH);
-                   }else{
-                       digitalWrite (seg7 [4] [j], LOW);
-                   }
-                   }
-
-                   int t_actual2 = millis();
-                   if(t_actual2 - t_previo2 <= t_delay2){
-
-                   }
-
-                  }
-
-
             }
         }
-        }
+    }
+
+                if(digitalRead (sensores [3])){       //si el sensor 4 esta encendido
+
+                    if(t_actual - t_previo <= t_delay){    //pasan 10 segundos, luego se muestra la letra C
+                        t_actual = t_previo;
+                    }
+                
+                if(t_actual2 - t_previo2 <= t_delay2){ // encinede "C" en el 7seg cada 500ms
+                    for(int i = 0; i<= 7; i++){
+                        if(seg7[6] [i]){
+                            digitalWrite(seg_init7 [i], HIGH);
+                        }else{
+                            digitalWrite(seg_init7 [i], LOW);
+                        }
+                        t_actual2 = t_previo2;
+                    }
+                }
+
+                if(t_actual2 - t_previo3 <= t_delay3){ // apaga "C" en el 7seg cada 500ms
+                    for(int i = 0; i<= 7; i++){
+                        digitalWrite(seg_init7 [i], LOW);
+                        t_actual2 = t_previo3;
+                    }
+                }
+                estado_de_produccion = 1;
+
+                while(digitalRead (sensores [0]) == 0){   //mientras no tenga nada en el primer sensor que muestre el GUION MEDIO
+                    for(int k = 0; k <= 7; k ++){
+                        if(seg7 [6] [k]){
+                            digitalWrite(seg_init7 [k], HIGH);
+                        }else{
+                            digitalWrite(seg_init7 [k], LOW);
+                        }
+                    }
+                }
+            }
+
     }
 }
